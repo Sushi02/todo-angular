4e186aec59efbc3a62d2f71d4330713d376904f5
Sushma Kodati <sushmakodati.95@gmail.com>
2014-05-26 22:22:53 +0530

    first working version

---
+++ mainController.js
@@ -0,0 +1,17 @@
+app.controller('mainController',function mainController($scope,todoStorage){
+	var todos=$scope.todos=todoStorage.get();
+	$scope.newTask=' ';
+	$scope.addNew=function(){
+		var newTask=$scope.newTask;//.trim();
+		if(newTask.length===0){
+			return;
+		}
+		todos.push({
+			title:newTask,
+			completed:false
+		});
+		todoStorage.put(todos);
+		localStorage.setItem('todo', JSON.stringify(todos));
+		//todoStorage.put(todos);
+	};
+})
---
+++ todoStorage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+app.factory('todoStorage', function () {
+	var STORAGE_ID = 'todo';
+
+	return {
+		get: function () {
+			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+		},
+
+		put: function (todos) {
+			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+		}
+	};
+});
